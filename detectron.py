e41d0f96135646c4bd6caec89829073b1d5eadae
matpetrone <matteo.petrone@icloud.com>
2020-03-19 16:50:47 +0100

    training

--- detectron.py
+++ detectron.py
@@ -29,11 +29,16 @@ cfg.DATASETS.TRAIN = ("plans",)
 cfg.DATASETS.TEST = ()
 cfg.DATALOADER.NUM_WORKERS = 4
 cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
-cfg.SOLVER.IMS_PER_BATCH = 16  #prima =2
-cfg.SOLVER.BASE_LR = 0.0001  # pick a good LR prima=0.00025 balloon , 0.02 nuts
-cfg.SOLVER.MAX_ITER = 400    # 300 iterations seems good enough for this toy dataset; you may need to train longer for a practical dataset
-cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 256   # faster, and good enough for this toy dataset (default: 512)
+cfg.SOLVER.IMS_PER_BATCH = 2  #prima =2
+cfg.SOLVER.BASE_LR = 0.0005  # pick a good LR prima=0.00025 balloon , 0.02 nuts
+cfg.SOLVER.MAX_ITER = 5000    # 300 iterations seems good enough for this toy dataset; you may need to train longer for a practical dataset
+cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   # faster, and good enough for this toy dataset (default: 512)
 cfg.MODEL.ROI_HEADS.NUM_CLASSES = 12  # only has one class (ballon)
+
+cfg.INPUT.MIN_SIZE_TRAIN=(1000,)
+cfg.INPUT.MIN_SIZE_TRAIN=1000
+cfg.INPUT.MIN_SIZE_TEST=1000
+cfg.INPUT.MIN_SIZE_TEST=1000
 
 os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
 trainer = DefaultTrainer(cfg)
@@ -46,8 +51,7 @@ cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5   # set the testing threshold for th
 cfg.DATASETS.TEST = ("plans",)
 predictor = DefaultPredictor(cfg)
 
-
-for d in random.sample(dataset_dicts, 3):
+for i,d in enumerate(random.sample(dataset_dicts, 150)):
     im = cv2.imread(d["file_name"])
     outputs = predictor(im)
     v = Visualizer(im[:, :, ::-1],
@@ -56,5 +60,7 @@ for d in random.sample(dataset_dicts, 3):
                    instance_mode=ColorMode.IMAGE_BW   # remove the colors of unsegmented pixels
     )
     v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-    cv2.imshow("1",v.get_image()[:, :, ::-1])
-    cv2.waitKey(delay=1)
+    path= './test'
+    #cv2.imshow("1",v.get_image()[:, :, ::-1])
+    cv2.imwrite(os.path.join(path, '{}.jpg'.format(i+1)), v.get_image()[:, :, ::-1])
+    #cv2.waitKey(delay=10000)
